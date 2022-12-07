b305db1c8de6105494418109917494b624773671
Veridiana Maricato <maricato.veridiana@gmail.com>
2022-12-07 10:01:43 -0300

    css finalizado; selecao DOM no js finalizada. min 35:02

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Selecao de elementos
+const todoForm = document.querySelector('#todo-form')
+const todoInput = document.querySelector('#todo-input')
+const todoList = document.querySelector('#todo-list')
+const editForm = document.querySelector('#edit-form')
+const editInput = document.querySelector('#edit-input')
+const cancelEditBtn = document.querySelector('#cancel-edit-btn')
+
+// Funcoes
+
+// Eventos
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const inputValue = todoInput.value
+
+    if(inputValue){
+        console.log(inputValue)
+    }
+})
